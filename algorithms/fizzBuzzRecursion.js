64282b685edaf7c038f2fda4f5a9a12c334d4aca
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-05-03 09:29:46 -0700

    adds FizzBuzz solution

--- algorithms/fizzBuzzRecursion.js
+++ algorithms/fizzBuzzRecursion.js
@@ -3,3 +3,21 @@
 // https://www.hackerrank.com/test/143hd7jsid6/questions/521e954e6ff11
 
 
+function fizzBuzz(n) {
+  // Write your code here
+  for (let i = 1; i <= n; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+      console.log('FizzBuzz');
+  } else if (i % 3 === 0 && i % 5 !== 0) {
+      console.log('Fizz');
+  } else if (i % 5 === 0 && i % 3 !==0 ) {
+      console.log('Buzz');
+  } else console.log(i);
+  }
+}
+
+function main() {
+  const n = parseInt(readLine().trim(), 10);
+
+  fizzBuzz(n);
+}
