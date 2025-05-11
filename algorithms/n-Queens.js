822c8b345cfb975a9a3b728821556ae3dfe5efa5
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-05-11 11:53:08 -0700

    another attempt;

--- algorithms/n-Queens.js
+++ algorithms/n-Queens.js
@@ -37,72 +37,169 @@ Constraints:
  * @return {string[][]}
  */
  var solveNQueens = function(n) {
-  let obj = {};
+  // let obj = {};
+  // let board = [];
+  // let results = [];
+
+  // const createBoard = () => {
+  //     obj = {};
+  //     for (let i = 1; i <=n; i++ ) {
+  //         obj[i] = []
+  //         for (let j = 1; j <=n; j++ ) {
+  //             obj[i].push(j);
+  //         }
+  //     }
+  // };
+
+  // const removeAvail = (row, col) => {
+  //     obj[row] = []; // remove row;
+  //     for (let i = col; i <= n; i++) {
+  //         obj[i].splice(obj[i].indexOf(col), 1); // removes all cols;
+  //         if (obj[row-i]) {
+  //             if (obj[row-i].indexOf(col+i) > -1) {
+  //                 obj[row-i].splice(obj[row-i].indexOf(col+i), 1)
+  //             }
+
+  //              if (obj[row-i].indexOf(col-i) > -1) {
+  //                 obj[row-i].splice(obj[row-i].indexOf(col-i), 1)
+  //             }
+  //         }
+
+  //          if (obj[row+i]) {
+  //             if (obj[row+i].indexOf(col+i) > -1) {
+  //                 obj[row+i].splice(obj[row+i].indexOf(col+i), 1)
+  //             }
+
+  //              if (obj[row+i].indexOf(col-i) > -1) {
+  //                 obj[row+i].splice(obj[row+i].indexOf(col-i), 1)
+  //             }
+  //         }
+  //     }
+  // };
+
+  // let counter = 1;
+
+  // placeQueens = (row, col) => {
+  //     if (obj[row] !== undefined) {
+  //         if (obj[row].indexOf(col) > -1) {
+              
+  //             removeAvail(row, col);
+              
+  //             let str = '';
+  //             for (let i = 1; i <=n; i++) {
+  //                 if (col === i) {
+  //                     str+= 'Q';
+  //                 } else {
+  //                     str += '.'
+  //                 }
+  //             }
+              
+  //             if (str.match('Q')) {
+  //                  board.push(str);
+  //             }
+
+  //             if (obj[row+1]){
+  //                 for (let i = 0; i < obj[row+1].length; i++) {
+  //                     placeQueens(row+1, obj[row+1][i]);
+  //                 }
+  //             }
+
+  //             results.push(board);
+  //             board = [];
+  //             createBoard(); 
+  //             placeQueens(1, counter++);
+  //         }
+  //     } 
+  // };
+
+  // createBoard();
+  // placeQueens(1, counter);
+  // return results //.filter(arr => arr.length === n);
+
   let board = [];
   let results = [];
+  let counter = 1;
 
-  const createBoard = () => {
-      for (let i = 1; i <=n; i++ ) {
-          obj[i] = []
-          for (let j = 1; j <=n; j++ ) {
-              obj[i].push(j);
-          }
+  const validSpace = (row, col) => {
+      
+      // array positions is different from number value;
+      rowPos = row-1;
+      colPos = col-1;
+
+      // row can't exists;
+      if (rowPos < 0) {
+          return false;
       }
+
+      // check if row exists;
+      if (board[rowPos]) { 
+          return false;
+      }
+
+      // check if column is taken;
+      for (let i = 0; i < board.lenght; i++) {
+          if (board[i].indexOf('Q') === colPos) {
+              return false;
+          }
+      };
+
+      // check if diagnals are taken;
+      for (let i = 1; i <= n; i++) {
+          if (board[rowPos-i]) {
+              if (board[rowPos-i].indexOf('Q') === colPos-1) {
+                  return false;
+              }
+          }
+
+          if (board[rowPos+i]) {
+              if (board[rowPos+1].indexOf('Q') === colPos+1) {
+                  return false;
+              }
+          }
+      };
+
+      return true;
   };
 
-  const removeAvail = (row, col) => {
-      obj[row] = []; // remove row;
-      for (let i = col; i <= n; i++) {
-          obj[i].splice(obj[i].indexOf(col), 1); // removes all cols;
-          if (obj[row-i]) {
-              if (obj[row-i].indexOf(col+i) > -1) {
-                  obj[row-i].splice(obj[row-i].indexOf(col+i), 1)
-              }
-
-               if (obj[row-i].indexOf(col-i) > -1) {
-                  obj[row-i].splice(obj[row-i].indexOf(col-i), 1)
-              }
-          }
-
-           if (obj[row+i]) {
-              if (obj[row+i].indexOf(col+i) > -1) {
-                  obj[row+i].splice(obj[row+i].indexOf(col+i), 1)
-              }
-
-               if (obj[row+i].indexOf(col-i) > -1) {
-                  obj[row+i].splice(obj[row+i].indexOf(col-i), 1)
-              }
-          }
-      }
-  };
-
-  placeQueens = (row, col) => {
-      if (obj[row] !== undefined) {
-          if (obj[row].indexOf(col) > -1) {
-              
-              removeAvail(row, col);
-              
-              let str = '';
-              for (let i = 1; i <=n; i++) {
+  const loop = (row, col) => {
+      if (board.length <= n) {
+          if (validSpace(row,col)) {
+              let str  = '';
+              for (let i = 0; i < n; i++) {
                   if (col === i) {
-                      str+= 'Q';
+                      str += 'Q';
                   } else {
-                      str += '.'
+                      str += '.';
                   }
               }
 
-              obj[row] = [];
-              obj[row].push(str);
-              placeQueens(row+1, obj[row+1][0]);
-          } else {
+              if (str.length === n) {
+                  board.push(str);
+              }
+
+              if (board.length === n) {
+                  results.push(board);
+              }
+
+              for (let i = 1; i <= n; i++) {
+                  loop(row+1, col+i);
+              }
+
+              if (board.length === n) {
+                  results.push(board);
+                  board = [];
+                  loop(1, counter++);
+              };
               
+          } else {
+
           }
-      }
+      };
   };
 
-  createBoard();
-  placeQueens(1, 1);
+  // loop(1, 1);
 
-  return obj;
-  // return results;
+  // board=['....']
+
+  // return validSpace(1,1);
 };
