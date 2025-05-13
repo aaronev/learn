652a6ef9c6aeb18f4246cf502983a648a0626756
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-05-12 20:50:58 -0700

    unsolved

--- algorithms/n-Queens.js
+++ algorithms/n-Queens.js
@@ -123,118 +123,96 @@ Constraints:
  */
 
  var solveNQueens = function(n) {
-  let results = [];
-
-  if ( n === 1) {
-      return [['Q']];
+  // JavaScript Program to solve the n-queens problem
+  
+  // Function to check if it is safe to place
+  // the queen at board[row][col]
+  function isSafe(mat, row, col) {
+  
+      // Check this col on upper side
+      for (let i = 0; i < row; i++)
+          if (mat[i][col] === 1)
+              return false;
+  
+      // Check upper diagonal on left side
+      for (let i = row - 1, j = col - 1; 
+            i >= 0 && j >= 0; i--, j--)
+          if (mat[i][j] === 1)
+              return false;
+  
+      // Check lower diagonal on left side
+      for (let i = row - 1, j = col + 1;
+            j < n && i >= 0; i--, j++)
+          if (mat[i][j] === 1)
+              return false;
+  
+      return true;
   }
-
-  const createBoard = () => {
-      let board = {};
-      let takenCols = [];
-      let takenRows = [];
-
+  
+  function placeQueens(row, mat) {
+  
+      // base case: If all queens are placed
+      // then return true
+      if (row === n)
+          return true;
+  
+      // Consider the row and try placing
+      // queen in all columns one by one
       for (let i = 0; i < n; i++) {
-          board[i] = "";
-      };
-
-      board.results = () => {
-          let arr = [];
-          for (let i = 0; i < n; i++) {
-              if (board[i] !== '') {
-                   arr.push(board[i]);
-              }
-          }
-          return arr;
-      };
-
-      board.add = (r, c) => {
-          takenRows.push(r);
-          takenCols.push(c);
-          let str = '';
-          for (let i = 0; i < n; i++) {
-              if (i === c) {
-                  str += 'Q';
-              } else {
-                  str += '.';
-              }
-          }
-
-          if (str.includes('Q')) {
-              board[r] += str;
-          } 
-      };
-
-      board.checkAvail = (r, c) => {
-          let checkAvail={};
-          checkAvail.bool = true;
-
-          if (takenCols.includes(c) || c >= n) {
-              checkAvail.bool = false;
-              checkAvail.reason = 'col';
-          }
-
-          if (takenRows.includes(r) || r >= n) {
-              checkAvail.bool = false;
-              checkAvail.reason = 'row';
-          }
-
-          for (let i = 0; i <= r ; i++) {
-              if (board[r-i]) {
-                  if (board[r-i].indexOf('Q') === c-i) {
-                      checkAvail.bool = false;
-                      checkAvail.reason = 'diaganal';
-                  }
-
-                   if (board[r-i].indexOf('Q') === c+i) {
-                      checkAvail.bool = false;
-                      checkAvail.reason = 'diaganal';
-                  }
-              }  
-          }
-          
-          return checkAvail;
-      };
-      return board;
-  };
-
-  const loop = (row, col, board) => {
-      if (row < n) {
-          let check = board.checkAvail(row, col, board);
-          if (check.bool) {
-              board.add(row, col);
-              for (let j = 0; j < n; j++) {
-                  loop(row, j, board);
-                  loop(row+j, 0, board);
-              }
-          } else {
-              for (let i = 0; i < n; i++) {
-                  let check = board.checkAvail(row, i)
-                  if (check.bool) {
-                      loop(row, i, board);
-                  } 
-              }
-          }
-      } else {
-          let finalResults = board.results();
-          if (finalResults.length === n) {
-              let notExist = true;
-              for (let i = 0; i < results.length; i++) {
-                  if (results[i].toString() === finalResults.toString()) {
-                      notExist = false;
-                  }
-              }
-              if (notExist) {
-                  results.push(finalResults);
-              }
-              
+  
+          // Check if the queen can be placed
+          if (isSafe(mat, row, i)) {
+              mat[row][i] = 1;
+              if (placeQueens(row + 1, mat))
+                  return true;
+              mat[row][i] = 0;
           }
       }
-  };
-
+      return false;
+  }
+  
+  // Function to find the solution
+  // to the N-Queens problem
+  function nQueen(n) {
+  
+      // Initialize the board
+      const mat = Array.from({ length: n }, 
+                () => Array(n).fill(0));
+  
+      // If the solution exists
+      if (placeQueens(0, mat)) {
+  
+          // to store the columns of the queens
+          const ans = [];
+          for (let i = 0; i < n; i++) {
+              for (let j = 0; j < n; j++) {
+                  if (mat[i][j] === 1) {
+                      ans.push(j + 1);
+                  }
+              }
+          }
+          return ans;
+      } else
+          return [-1];
+  }
+  
+  const ans = nQueen(n);
+  
+  let results = [];
+  let b = [];
+  
   for (let i = 0; i < n; i++) {
-      loop(0, i, createBoard());
+      let str = '';
+      for (let j = 0; j < n; j++) {
+          if (ans[i] === j) {
+              str += 'Q';
+          } else {
+              str += '.';
+          }
+      }
+      b.push(str);
+      results.push(b);
   };
   
-  return results;
-};
+  return results
+  };
