e565453b49ced1d903903a6eb85eaaa010d24731
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-05-24 14:40:09 -0700

    attempt submit 1

--- algorithms/medianOf2SortedArrays.js
+++ algorithms/medianOf2SortedArrays.js
@@ -28,3 +28,23 @@ nums2.length == n
 */
 
 // https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+ var findMedianSortedArrays = function(nums1, nums2) {
+  let sum = 0;
+
+  const add = (arr) => {
+      for (let i = 0; i < arr.length; i++) {
+          sum += arr[i]
+      }
+  }
+
+  add(nums1);
+  add(nums2);
+
+  return sum / (nums1.length + nums2.length);
+};
