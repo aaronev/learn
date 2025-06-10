712a1d322416e80b1049a313dc125d64dec580f8
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-06-10 13:14:31 -0700

    adds solution

--- algorithms/clap.js
+++ algorithms/clap.js
@@ -44,3 +44,19 @@ Get 30% off now with BETA30 →
 */
 
 // https://www.greatfrontend.com/questions/javascript/clamp?practice=practice&tab=coding
+
+/**
+ * @param {number} value The number to clamp.
+ * @param {number} lower The lower bound.
+ * @param {number} upper The upper bound.
+ * @returns {number} Returns the clamped number.
+ */
+ export default function clamp(value, lower, upper) {
+  if (value > upper) {
+    return upper;
+  } else if (value < lower) {
+    return lower;
+  } else {
+    return value;
+  }
+}
