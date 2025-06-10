36ff8733bbe409d088458c7f1a5dca6f921cde09
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-06-10 13:13:27 -0700

    adds discription

---
+++ algorithms/clap.js
@@ -0,0 +1,44 @@
+/*
+Clamp
+Premium
+Languages
+Difficulty
+Easy
+Recommended duration to spend during interviews
+5mins
+Users completed
+7.11k done
+Implement a function clamp(number, lower, upper) to restrict a number within the inclusive lower and upper bounds.
+
+Arguments
+value (number): The number to clamp.
+lower (number): The lower bound.
+upper (number): The upper bound.
+Returns
+(number): Returns the clamped number.
+
+Examples
+// Within the bounds, return as-is.
+clamp(3, 0, 5); // => 3
+
+// Smaller than the lower bound.
+clamp(-10, -3, 5); // => -3
+
+// Bigger than the upper bound.
+clamp(10, -5, 5); // => 5
+Resources
+Lodash _.clamp
+Similar questions
+In Range
+Difficulty
+Easy
+Sponsor: GreatFrontEnd Projects
+The fastest way to learn Front End – by building actual projects
+Want to grow in your front end skills? Learn them the right way — build apps step-by-step while referencing best practices from Senior engineers.
+
+80+ real world projects
+Professional specs and designs by top-tier product managers and designers
+Guides and reference solutions from Senior Big Tech engineers
+Use a front end skill roadmap to build from beginner to pro
+Get 30% off now with BETA30 →
+*/
