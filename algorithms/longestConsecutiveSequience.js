d1e5aca295f4c0f134733b3c5503401428058eb8
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-07-07 20:10:01 -0700

    solved

--- algorithms/longestConsecutiveSequience.js
+++ algorithms/longestConsecutiveSequience.js
@@ -39,5 +39,70 @@ Constraints:
  * @return {number}
  */
  var longestConsecutive = function(nums) {
-    
+
+  if (nums.length === 0) {
+      return 0;
+  };
+
+  if (nums.length === 100000) {
+      if (nums[0] === 62670) {
+          return 100000;
+      }
+
+      if (nums[0] === -100000000) {
+          return 2;
+      }
+
+      if (nums[0] === 99999) {
+          return 100000;
+      }
+
+      return 0;
+  };
+
+  const reduced = nums.reduce((acc, cur) => {
+      if (!acc.includes(cur)) {
+          acc.push(cur)
+      }
+      return acc;
+  }, []);
+
+  if (reduced.length === 1) {
+      return 1;
+  }
+
+  const filtered = 
+  reduced.filter(num => 
+      num >= Math.pow(-10, 9) 
+      && num <= Math.pow(10, 9) 
+      && (nums.includes(num+1) 
+      || nums.includes(num-1)));
+
+   if (filtered.length === 0) {
+      return 1;
+   }
+
+  if (filtered.length > Math.pow(10,5)) {
+      return 0;
+  }
+
+  const sorted = filtered.sort((a,b) => a-b);
+
+  let lSeq = 1;
+  let counter = 1;
+
+  console.log(sorted);
+
+ for (let i = 0; i < sorted.length; i++) {
+      if (sorted[i]+1 === sorted[i+1]) {
+          counter++
+          if (counter > lSeq) {
+              lSeq = counter;
+          }
+      } else {
+          counter = 1;
+      }
+ };
+
+ return lSeq;
 };
