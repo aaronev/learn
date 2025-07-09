8bdf93be1318b12cedd6633db91285a684872d99
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-07-09 12:37:36 -0700

    adds initial set up

--- algorithms/containerWithMostWater.js
+++ algorithms/containerWithMostWater.js
@@ -36,3 +36,10 @@ n == height.length
 
 // https://leetcode.com/problems/container-with-most-water/description/
 
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+ var maxArea = function(height) {
+    
+};
