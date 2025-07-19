f27570299fea1c8438a4e1215c850e27264b1876
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-07-18 18:41:21 -0700

    adds set up

---
+++ algorithms/threeSumClosest.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var threeSumClosest = function(nums, target) {
+  nums.sort((a,b)=> b-a);
+
+  let res = Math.abs(nums[0]);
+  
+  if (res === target) {
+      return target;
+  }
+
+  for (let i = 0; i < nums.length-2; i++) {
+      let counter = i+1;
+      while(nums[counter] && nums[counter+1]) {
+          let theSum = nums[i] + nums[counter] +nums[counter+1];
+          console.log(theSum)
+          if (theSum === target) {
+              return target;
+          }
+          
+          if (Math.abs(theSum) < res) {
+              res = theSum;
+          }
+          counter++;
+      };
+  };
+
+  return res;
+};
