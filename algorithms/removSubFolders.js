dea8b2caa680d74d968d2f289fd65012df665908
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-07-19 21:32:36 -0700

    solved

--- algorithms/removSubFolders.js
+++ algorithms/removSubFolders.js
@@ -45,31 +45,6 @@
  */
  var removeSubfolders = function(folder) {
   // sorted the folder from short length to longest length
-  folder.sort((a,b) => a.length-b.length);
-  const results = [folder[0]];
-  
-  function removeMatch(str, ind) {
-      for (let i = 0; i < folder.length; i++) {
-          if (folder[i].substring(0, str.length) === str && folder[i].substring(0, str.length+1) === `${str}/`) {
-              folder.splice(folder.indexOf(folder[i]), 1);
-              i=0;
-          }
-      }
-  };
-
-  // function removeMatch(str, ind) {
-  //     for (let i = ind+1; i < folder.length; i++) {
-  //         if (!folder[i].substring(0, str.length) === str 
-  //         && folder[i].substring(0, str.length+1) === `${str}/`) {
-  //             results.push(folder[i]);
-  //         }
-  //     }
-  // };
-
-
-  for (let i = 0; i < folder.length; i++) {
-      removeMatch(folder[i]);
-  }
-
-  return folder;
+  let s = folder[1];
+  return folder.sort().filter(x => !x.startsWith(s + '/') && (s = x));
 };
