8dce754263260c8b82aa36d031e7c782d4525357
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-07-28 20:02:29 -0700

    interaces with the same name merges

--- typescript/interfaces.ts
+++ typescript/interfaces.ts
@@ -5,3 +5,18 @@ interface Authenticatable {
   login() : void;
   logout(): void;
 };
+
+let user: Authenticatable;
+
+user = {
+  email: 'test@example.com',
+  password: 'abc1',
+
+  login() {
+   // reach out to database, check credentials, create a session 
+  },
+  logout() {
+    // clear the session
+  },
+};
+
