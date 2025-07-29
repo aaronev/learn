76304d1cb6a47630336517c384b93fb4245c69b4
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-07-29 16:34:29 -0700

    solved

--- algorithms/4sum.ts
+++ algorithms/4sum.ts
@@ -35,4 +35,46 @@ Constraints:
 
 function fourSum(nums: number[], target: number): number[][] {
 
+
+  if (nums.length < 4) {
+      return [];
+  }
+
+  nums.sort();
+  let i = 0;
+  const result: number[][] = [];
+  const found: string[] = [];
+
+  const unique = nums.filter((val,ind, arr) => arr.indexOf(val) === ind, []);
+
+  if (unique.length === 1) {
+      if (target/4 === unique[0]) {
+          return [[unique[0], unique[0], unique[0], unique[0]]];
+      }
+  }
+
+
+  while (i < nums.length) {
+      let j = i+1;
+      while (j < nums.length) {
+          let k = j+1;
+          while (k < nums.length) {
+              let l = k+1;
+              while (l < nums.length) {
+                  if (nums[i]+ nums[j] + nums[k] + nums[l] === target) {
+                      if (!found.includes(`${[nums[i],nums[j],nums[k],nums[l]]}`)) {
+                          found.push(`${[nums[i],nums[j],nums[k],nums[l]]}`);
+                          result.push([nums[i],nums[j],nums[k],nums[l]]);
+                      }
+                  } 
+                  l++;
+              };
+              k++
+          };
+          j++
+      };
+      i++  
+  };
+  
+  return result
 };
