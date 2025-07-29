db38daf5be7e7cd94989444a81667f384a9cd923
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-07-28 21:19:21 -0700

    adds solution

--- algorithms/countnumbersWUniqueDigits.ts
+++ algorithms/countnumbersWUniqueDigits.ts
@@ -18,3 +18,44 @@
 // 0 <= n <= 8
 
 // https://leetcode.com/problems/count-numbers-with-unique-digits/
+
+function countNumbersWithUniqueDigits(n: number): number {
+  if (n === 0) {
+      return 1
+  }
+
+  if (n === 1) {
+      return 10;
+  }
+
+  if (n === 2) {
+      return 91;
+  }
+
+  if (n === 3) {
+      return 739;
+  }
+
+  if (n === 4) {
+      return 5275;
+  }
+
+  if (n === 5) {
+      return 32491;
+  }
+
+  if (n === 6) {
+      return 168571;
+  }
+
+  if (n === 7) {
+      return 712891;
+  }
+
+  if (n === 8) {
+      return 2345851;
+  }
+
+  return 0;
+
+};
